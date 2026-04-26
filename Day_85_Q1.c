22e6c06a241b0e175859b7dd176c03c3dacdff66
Karmanya Maletha <karmanyamaletha@gmail.com>
2026-04-26 15:17:48 +0530

    Implement Merge Sort

---
+++ Day_85_Q1.c
@@ -0,0 +1,117 @@
+/*153. Find Minimum in Rotated Sorted Array
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ */
+
+ 
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int mid = left + (right - left)/2;
+
+        mergeSort(arr, left, mid);      
+        mergeSort(arr, mid + 1, right); 
+
+        merge(arr, left, mid, right); 
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    mergeSort(arr, 0, n-1);
+
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
