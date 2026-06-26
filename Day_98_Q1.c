a9130cfda1e0a269668c5ce6cd749d34854abd27
Karmanya Maletha <karmanyamaletha@gmail.com>
2026-06-26 12:37:59 +0530

    Interval Merging
    This program merges overlapping intervals provided by the user.

---
+++ Day_98_Q1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int start;
+    int end;
+} Interval;
+
+// Comparator to sort intervals by start time
+int compare(const void *a, const void *b)
+{
+    Interval *i1 = (Interval *)a;
+    Interval *i2 = (Interval *)b;
+    return i1->start - i2->start;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of intervals: ");
+    scanf("%d", &n);
+
+    Interval arr[n];
+
+    printf("Enter start and end of each interval:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d %d", &arr[i].start, &arr[i].end);
+
+    // Sort intervals by start time
+    qsort(arr, n, sizeof(Interval), compare);
+
+    printf("\nMerged Intervals:\n");
+
+    int start = arr[0].start;
+    int end = arr[0].end;
+
+    for (int i = 1; i < n; i++)
+    {
+
+        if (arr[i].start <= end)
+        {
+            if (arr[i].end > end)
+                end = arr[i].end;
+        }
+        else
+        {
+         
+            printf("[%d, %d]\n", start, end);
+
+            start = arr[i].start;
+            end = arr[i].end;
+        }
+    }
+
+ 
+    printf("[%d, %d]\n", start, end);
+
+    return 0;
+}
